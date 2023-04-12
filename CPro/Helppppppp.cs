d2d71ba19db20f44e7ee747a3a19fd57573f5d76
Андрей Будяну <andre40000k@gmail.com>
2023-04-12 20:17:56 +0300

    Removed unused "using"

--- CPro/Helppppppp.cs
+++ CPro/Helppppppp.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CPro_1
+﻿namespace CPro_1
 {
     public static class Helppppppp
     {
@@ -12,6 +6,7 @@ namespace CPro_1
         {
             return new string('*', numStars);
         }
+
         public static void ShowInformation(Transport transport)
         {
             transport.ShowInfo();
